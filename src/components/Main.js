0be0e46132c957a827a3d6d6db693ba9618cd74d
tarun2506 <tarunsharma8920@gmail.com>
2021-07-28 20:18:38 +0530

    changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/main.css";
 function Main() {
+  const [text, setText] = useState("");
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
   return (
     <div className="main">
       <div className="input-container">
-        <h2>Editor</h2>
-        <textarea id="editor" cols="50" rows="20"></textarea>
+        <h1>Editor</h1>
+        <textarea id="editor" onChange={handleText}></textarea>
       </div>
       <div className="output-container">
-        <h2>Previewer</h2>
-        <div id="preview">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, deleniti
-          totam delectus inventore assumenda odit dolorum quibusdam! Omnis
-          explicabo corporis fugit quia culpa at veritatis. Veritatis cupiditate
-          voluptatibus doloribus fugit?
-        </div>
+        <h1>Previewer</h1>
+        <p id="preview">{text}</p>
       </div>
     </div>
   );
